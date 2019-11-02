a97a406828148c8822652182d534616f1c8bf24c
moqiaoduo <moqiaoduo@gmail.com>
2019-11-02 16:22:39 +0800

    upgrade

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"github.com/akkuman/parseConfig"
 	"github.com/gin-gonic/gin"
 	"google.golang.org/grpc"
 	"log"
@@ -26,19 +28,85 @@ const (
 	ShellToUse = "bash"
 )
 
+var (
+	accessKey string
+	users     []V2RayUser
+	cmdConn   *grpc.ClientConn
+)
+
+type V2RayUser struct {
+	Email string `json:"email"`
+	UUID  string `json:"uuid"`
+	Level int    `json:"level"`
+}
+
 func main() {
+	var config = parseConfig.New("/etc/v2ray/api_config.json")
+	accessKeyI := config.Get("key")
+	if accessKeyI == nil {
+		fmt.Println("No access key set. Abort.")
+		os.Exit(1)
+	}
+	accessKey = accessKeyI.(string)
 	var address, msg string
+	var err error
 	success := false
 	r := gin.Default()
-	cmdConn, err := grpc.Dial(fmt.Sprintf("%s:%d", ApiAddress, ApiPort), grpc.WithInsecure())
+	cmdConn, err = grpc.Dial(fmt.Sprintf("%s:%d", ApiAddress, ApiPort), grpc.WithInsecure())
 	if err != nil {
 		panic(err)
 	}
+	r.Use(webMiddleware)
+	r.POST("/sync", func(c *gin.Context) {
+		var newList []V2RayUser
+		var result []string
+		err2 := json.Unmarshal([]byte(c.PostForm("data")), &newList)
+		if err2 != nil {
+			respondWithError(200, "Couldn't parse JSON data", c)
+			return
+		}
+		if len(users) > 0 {
+			result = sync(users, newList)
+		} else {
+			for i := 0; i < len(newList); i++ {
+				resp, err3 := addUser(cmdConn, &newList[i])
+				if err3 == nil {
+					msg := "Add User [" + newList[i].Email + "] OK: " + resp.String()
+					fmt.Println(msg)
+					result = append(result, msg)
+				} else {
+					msg := "Add User [" + newList[i].Email + "] ERR: " + err3.Error()
+					fmt.Println(msg)
+					result = append(result, msg)
+				}
+			}
+		}
+		users = newList
+		c.JSON(200, gin.H{
+			"success": true,
+			"msg":     result,
+		})
+	})
 	r.POST("/addUser", func(c *gin.Context) {
 		success = false
 		level, _ := strconv.Atoi(c.PostForm("level"))
-		resp, _ := addUser(cmdConn, uint32(level), c.PostForm("email"), c.PostForm("uuid"))
-		msg = resp.String()
+		email := c.PostForm("email")
+		if email == "" {
+			respondWithError(200, "Email Required", c)
+			return
+		}
+		UUID := c.PostForm("email")
+		if UUID == "" {
+			respondWithError(200, "UUID Required", c)
+			return
+		}
+		resp, err2 := addUser(cmdConn, &V2RayUser{email, UUID, level})
+		if err2 == nil {
+			success = true
+			msg = resp.String()
+		} else {
+			msg = err2.Error()
+		}
 		c.JSON(200, gin.H{
 			"success": success,
 			"msg":     msg,
@@ -46,8 +114,18 @@ func main() {
 	})
 	r.POST("/delUser", func(c *gin.Context) {
 		success = false
-		resp, _ := removeUser(cmdConn, c.PostForm("email"))
-		msg = resp.String()
+		email := c.PostForm("email")
+		if email == "" {
+			respondWithError(200, "Email Required", c)
+			return
+		}
+		resp, err2 := removeUser(cmdConn, &V2RayUser{Email: email})
+		if err2 == nil {
+			success = true
+			msg = resp.String()
+		} else {
+			msg = err2.Error()
+		}
 		c.JSON(200, gin.H{
 			"success": success,
 			"msg":     msg,
@@ -55,28 +133,109 @@ func main() {
 	})
 	r.POST("/traffic", func(c *gin.Context) {
 		reset, _ := strconv.ParseBool(c.DefaultPostForm("reset", "false"))
-		resp, _ := traffic(cmdConn, reset)
-		msg = resp.String()
+		resp, err2 := traffic(cmdConn, reset)
+		if err2 == nil {
+			success = true
+			msg = resp.String()
+		} else {
+			msg = err2.Error()
+		}
 		c.JSON(200, gin.H{
 			"success": success,
 			"msg":     msg,
 		})
 	})
-	args := os.Args[1:]
-	if len(args) >= 1 {
-		address = args[0]
-	} else {
+	r.POST("/reboot", func(c *gin.Context) {
+		err, okS, errS := shell("systemctl restart v2ray")
+		if err == nil {
+			msg = okS
+			success = true
+		} else {
+			msg = errS
+			success = false
+		}
+		c.JSON(200, gin.H{
+			"success": success,
+			"msg":     msg,
+		})
+	})
+	addressI := config.Get("address")
+	if addressI == nil {
 		address = "127.0.0.1:8080"
+	} else {
+		address = addressI.(string)
 	}
-	r.Use(webMiddleware)
 	_ = r.Run(address)
+}
+
+func sync(oldList []V2RayUser, newList []V2RayUser) []string {
+	var base []V2RayUser
+	var result []string
+	for i := 0; i < len(oldList); i++ {
+		found := false
+		for j := 0; j < len(newList); j++ {
+			if oldList[i] == newList[j] {
+				found = true
+				break
+			}
+		}
+		if found {
+			base = append(base, oldList[i])
+		} else {
+			resp, err := removeUser(cmdConn, &oldList[i])
+			if err == nil {
+				msg := "Remove User [" + oldList[i].Email + "] OK: " + resp.String()
+				fmt.Println(msg)
+				result = append(result, msg)
+			} else {
+				msg := "Remove User [" + oldList[i].Email + "] ERR: " + err.Error()
+				fmt.Println(msg)
+				result = append(result, msg)
+			}
+		}
+	}
+	for i := 0; i < len(newList); i++ {
+		found := false
+		for j := 0; j < len(base); j++ {
+			if newList[i] == base[j] {
+				found = true
+				break
+			}
+		}
+		if !found {
+			resp, err := addUser(cmdConn, &newList[i])
+			if err == nil {
+				msg := "Add User [" + newList[i].Email + "] OK: " + resp.String()
+				fmt.Println(msg)
+				result = append(result, msg)
+			} else {
+				msg := "Add User [" + newList[i].Email + "] ERR: " + err.Error()
+				fmt.Println(msg)
+				result = append(result, msg)
+			}
+		}
+	}
+	return result
 }
 
 func webMiddleware(c *gin.Context) {
 	if c.PostForm("key") == "" {
-
+		respondWithError(401, "API token required", c)
+		return
+	}
+	if c.PostForm("key") != accessKey {
+		respondWithError(403, "API token incorrect", c)
+		return
 	}
 	c.Next()
+}
+
+func respondWithError(code int, message string, c *gin.Context) {
+	c.JSON(code, gin.H{
+		"success": false,
+		"msg":     message,
+	})
+	c.Abort()
 }
 
 func shell(command string) (error, string, string) {
@@ -89,16 +248,16 @@ func shell(command string) (error, string, string) {
 	return err, stdout.String(), stderr.String()
 }
 
-func addUser(cmdConn *grpc.ClientConn, level uint32, email string, UUID string) (*command.AlterInboundResponse, error) {
+func addUser(cmdConn *grpc.ClientConn, user *V2RayUser) (*command.AlterInboundResponse, error) {
 	c := command.NewHandlerServiceClient(cmdConn)
 	resp, err := c.AlterInbound(context.Background(), &command.AlterInboundRequest{
 		Tag: InboundTag,
 		Operation: serial.ToTypedMessage(&command.AddUserOperation{
 			User: &protocol.User{
-				Level: level,
-				Email: email,
+				Level: uint32(user.Level),
+				Email: user.Email,
 				Account: serial.ToTypedMessage(&vmess.Account{
-					Id:               UUID,
+					Id:               user.UUID,
 					AlterId:          ALTERID,
 					SecuritySettings: &protocol.SecurityConfig{Type: protocol.SecurityType_AUTO},
 				}),
@@ -112,12 +271,12 @@ func addUser(cmdConn *grpc.ClientConn, level uint32, email string, UUID string) 
 	}
 	return resp, err
 }
-func removeUser(cmdConn *grpc.ClientConn, email string) (*command.AlterInboundResponse, error) {
+func removeUser(cmdConn *grpc.ClientConn, user *V2RayUser) (*command.AlterInboundResponse, error) {
 	c := command.NewHandlerServiceClient(cmdConn)
 	resp, err := c.AlterInbound(context.Background(), &command.AlterInboundRequest{
 		Tag: InboundTag,
 		Operation: serial.ToTypedMessage(&command.RemoveUserOperation{
-			Email: email,
+			Email: user.Email,
 		}),
 	})
 	if err != nil {
